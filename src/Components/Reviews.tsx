5805d4b24e4d9c5917c1c6dafeb9311d4c5bfc0e
unknown <rafaulgoni787898@gmail.com>
2024-09-19 04:17:29 +0600

    add title and icon

---
+++ src/Components/Reviews.tsx
@@ -0,0 +1,22 @@
+const Reviews: React.FC = () => {
+    return (
+        <div className='bg-[#111010]'>
+            <div className="container mx-auto pt-16">
+                <div className='border-l-4 border-amber-600 pb-2'>
+                    <div className='ml-2 space-y-1'>
+                        <p className='text-[#fff] font-semibold'>Testimonials</p>
+                        <h1 className='text-4xl text-amber-600 font-bold'>What Clients Say</h1>
+                        <p className='max-w-[600px] text-[#88837a]'>Client reviews drive UrbanDrive to deliver better and more efficient service.</p>
+                    </div>
+                </div>
+                <div className="pt-8 pb-8">
+                    
+                    <p>Rafaul</p>
+
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Reviews
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import BookAutoRental from "../../Components/BookAutoRental";
 import CarRental from "../../Components/CarRental";
+import Reviews from "../../Components/Reviews";
 
 const Home: React.FC = () => {
   return (
     <div>
       <CarRental></CarRental>
       <BookAutoRental></BookAutoRental>
+      <Reviews></Reviews>
     </div>
   );
 }
