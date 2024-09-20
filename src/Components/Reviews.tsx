a5370f83a783a4733ff550eec75863665caca72a
unknown <rafaulgoni787898@gmail.com>
2024-09-20 12:17:22 +0600

    solve issue

--- src/Components/Reviews.tsx
+++ src/Components/Reviews.tsx
@@ -2,30 +2,35 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { Rating } from '@smastrom/react-rating';
 import '@smastrom/react-rating/style.css';
+import Aayman from '../assets/revi/aayman.jpg'
+import Ruksana from '../assets/revi/juksana.jpg'
+import Najmul from '../assets/revi/nasmul.jpg'
+import Nayem from '../assets/revi/nayem.jpg'
+import Zihad from '../assets/revi/zahid.jpg'
 
 // Define the shape of a review object
-interface Review {
-  _id: string;
-  image: string;
-  name: string;
-  details: string;
-  rating: number;
-}
+// interface Review {
+//   _id: string;
+//   image: string;
+//   name: string;
+//   details: string;
+//   rating: number;
+// }
 
 const Reviews: React.FC = () => {
-  const [reviews, setReviews] = useState<Review[]>([]); // Set the type of reviews
+  // const [reviews, setReviews] = useState<Review[]>([]); // Set the type of reviews
 
-  useEffect(() => {
-    fetch('https://b9a12-server-side-rafaulgoni.vercel.app/reviews')
-      .then(res => res.json())
-      .then((data: Review[]) => { // Explicitly typing the data from API response
-        setReviews(data);
-      })
-      .catch(err => console.error('Error fetching reviews:', err));
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://b9a12-server-side-rafaulgoni.vercel.app/reviews')
+  //     .then(res => res.json())
+  //     .then((data: Review[]) => { // Explicitly typing the data from API response
+  //       setReviews(data);
+  //     })
+  //     .catch(err => console.error('Error fetching reviews:', err));
+  // }, []);
 
   return (
     <div className='bg-[#111010] mt-10'>
@@ -42,7 +47,109 @@ const Reviews: React.FC = () => {
         <div className="pt-4 pb-4">
           <div className='mt-5'>
             <Swiper navigation={true} loop={true} modules={[Navigation]} className="mySwiper">
-              {
+
+
+              <SwiperSlide>
+              <div className="card md:card-side bg-base-100 border max-w-[900px] mx-auto">
+                      <figure className='w-96 mx-auto'>
+                        <img src={Aayman} alt={'Aayman Alfee'} />
+                      </figure>
+                      <div className="card-body">
+                        <Rating
+                          style={{ maxWidth: 180 }}
+                          value={5}
+                          readOnly
+                        />
+                        <h2 className="card-title text-3xl"><span className='font-bold'>Name:</span> {'Aayman Alfee'}</h2>
+                        <p>
+                          <span className='font-bold text-amber-600'>Clients Say: </span>
+                          <br />
+                          <span className='text-[#88837a]'>{"This is by far the best service I have ever used. The customer support is outstanding, and the product itself is top-notch. I couldn't be happier!"}</span>
+                        </p>
+                      </div>
+                    </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="card md:card-side bg-base-100 border max-w-[900px] mx-auto">
+                      <figure className='w-96 mx-auto'>
+                        <img src={Ruksana} alt={'Roksana Sikder'} />
+                      </figure>
+                      <div className="card-body">
+                        <Rating
+                          style={{ maxWidth: 180 }}
+                          value={4}
+                          readOnly
+                        />
+                        <h2 className="card-title text-3xl"><span className='font-bold'>Name:</span> {'Roksana Sikder'}</h2>
+                        <p>
+                          <span className='font-bold text-amber-600'>Clients Say: </span>
+                          <br />
+                          <span className='text-[#88837a]'>{"UrbanDrive best service I have ever used. The customer support is outstanding, and the product itself is top-notch. I couldn't be happier!"}</span>
+                        </p>
+                      </div>
+                    </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="card md:card-side bg-base-100 border max-w-[900px] mx-auto">
+                      <figure className='w-96 mx-auto'>
+                        <img src={Najmul} alt={'Nazmul Hossain'} />
+                      </figure>
+                      <div className="card-body">
+                        <Rating
+                          style={{ maxWidth: 180 }}
+                          value={5}
+                          readOnly
+                        />
+                        <h2 className="card-title text-3xl"><span className='font-bold'>Name:</span> {'Nazmul Hosan'}</h2>
+                        <p>
+                          <span className='font-bold text-amber-600'>Clients Say: </span>
+                          <br />
+                          <span className='text-[#88837a]'>{"UrbanDrive best service I have ever used. The customer support is outstanding, and the product itself is top-notch. I couldn't be happier!"}</span>
+                        </p>
+                      </div>
+                    </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="card md:card-side bg-base-100 border max-w-[900px] mx-auto">
+                      <figure className='w-96 mx-auto'>
+                        <img src={Nayem} alt={'NA YE EM'} />
+                      </figure>
+                      <div className="card-body">
+                        <Rating
+                          style={{ maxWidth: 180 }}
+                          value={5}
+                          readOnly
+                        />
+                        <h2 className="card-title text-3xl"><span className='font-bold'>Name:</span> {'NA YE EM'}</h2>
+                        <p>
+                          <span className='font-bold text-amber-600'>Clients Say: </span>
+                          <br />
+                          <span className='text-[#88837a]'>{"UrbanDrive best service I have ever used. The customer support is outstanding, and the product itself is top-notch. I couldn't be happier!"}</span>
+                        </p>
+                      </div>
+                    </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="card md:card-side bg-base-100 border max-w-[900px] mx-auto">
+                      <figure className='w-96 mx-auto'>
+                        <img src={Zihad} alt={'Md Zihad'} />
+                      </figure>
+                      <div className="card-body">
+                        <Rating
+                          style={{ maxWidth: 180 }}
+                          value={4}
+                          readOnly
+                        />
+                        <h2 className="card-title text-3xl"><span className='font-bold'>Name:</span> {'Zihad Hasan'}</h2>
+                        <p>
+                          <span className='font-bold text-amber-600'>Clients Say: </span>
+                          <br />
+                          <span className='text-[#88837a]'>{"UrbanDrive best service I have ever used. The customer support is outstanding, and the product itself is top-notch. I couldn't be happier!"}</span>
+                        </p>
+                      </div>
+                    </div>
+              </SwiperSlide>
+              {/* {
                 reviews.map(review => (
                   <SwiperSlide key={review._id}>
                     <div className="card md:card-side bg-base-100 border max-w-[900px] mx-auto">
@@ -59,13 +166,13 @@ const Reviews: React.FC = () => {
                         <p>
                           <span className='font-bold text-amber-600'>One of the best words ever written for me:</span>
                           <br />
-                          <span className='text-black'>{review.details}</span>
+                          <span className='text-[#88837a]'>{review.details}</span>
                         </p>
                       </div>
                     </div>
                   </SwiperSlide>
                 ))
-              }
+              } */}
             </Swiper>
           </div>
         </div>
