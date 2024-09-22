2c691af3b4f297fcb03a0b02773d3405e1e46017
Armstain <tahsin10@outlook.com>
2024-09-22 23:21:47 +0600

    adjust responsiveness

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -43,7 +43,7 @@ const Contact: React.FC = () => {
           </div>
         </div>
         
-        <div className='absolute bottom-0 left-0 right-0 transform translate-y-1/2'>
+        <div className='lg:absolute lg:bottom-0 lg:left-0 lg:right-0 lg:transform lg:translate-y-1/2'>
           <div className='container mx-auto px-4'>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6'>
               {contactInfo.map((item, index) => (
@@ -164,7 +164,7 @@ const Contact: React.FC = () => {
           Don't hesitate and send us a message.
           </p>
 
-          <div className="flex justify-center mt-8 space-x-4">
+          <div className="flex justify-center mt-8 pb-8 space-x-4">
   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-500 to-navy-700 group-hover:from-teal-500 group-hover:to-navy-700 hover:text-white dark:text-white focus:ring-2 focus:outline-none focus:ring-white dark:focus:ring-white">
     <span className="relative px-3 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 font-Open">
       Live Chat    
@@ -172,7 +172,7 @@ const Contact: React.FC = () => {
   </button>
 
   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-teal-500 rounded-lg group hover:text-white border border-teal-500 focus:ring-2 focus:outline-none focus:ring-white dark:focus:ring-white">
-    <span className="relative px-3 py-2 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-teal-500 font-Open">
+    <span className="relative px-3 py-1 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-teal-500 font-Open">
       Rent Now
     </span>
   </button>
