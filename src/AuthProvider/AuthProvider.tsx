335e785697ef44db53b21159fbbf5c07183c352a
Armstain <tahsin10@outlook.com>
2024-09-27 02:38:05 +0600

    redesigned section

--- src/AuthProvider/AuthProvider.tsx
+++ src/AuthProvider/AuthProvider.tsx
@@ -87,19 +87,19 @@ const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      // if (currentUser) {
-      //   // get token from client site and store it
-      //   const userInfo = { email: currentUser.email };
-      //   axiosPublic.post("/jwt", userInfo).then((res) => {
-      //     if (res.data.token) {
-      //       localStorage.setItem("access-token", res.data.token);
-      //       setLoading(false);
-      //     }
-      //   });
-      // } else {
-      //   localStorage.removeItem("access-token");
-      //   setLoading(false);
-      // }
+      if (currentUser) {
+        // get token from client site and store it
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+            setLoading(false);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+        setLoading(false);
+      }
       setLoading(false)
     });
     return () => {
