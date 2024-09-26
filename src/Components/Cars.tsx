de209f2e9eff77b7ace0ab41694004e156e341e8
roksana35 <roksana7235@gmail.com>
2024-09-26 14:34:58 +0600

    design the card data

--- src/Components/Cars.tsx
+++ src/Components/Cars.tsx
@@ -1,7 +1,10 @@
 
 import useAxiosPublic from '../Hooks/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
-
+import { MdOutlineDiscount } from "react-icons/md";
+import { MdOutlineStar } from "react-icons/md";
+import { FaAward } from "react-icons/fa";
+import { FaMapLocationDot } from "react-icons/fa6";
 
 import { useEffect, useState } from 'react';
 
@@ -24,7 +27,12 @@ const Cars: React.FC = () => {
         model:string,
         category:string,
         price:number,
-       date: number
+       date: number,
+       description:string,
+       discount:number,
+       rating:number,
+       trip_count:number,
+       make:string
     }
  
 
@@ -77,16 +85,14 @@ const Cars: React.FC = () => {
         }
       };
   return (
-    <div className='bg-[#181818]  mt-16'>
-    
-    <div>
-    
-             <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 lg:gap-12'>
-             <select className="select w-full bg-[#111010] text-white  border rounded-full p-3 h-12"
+    <div className='mt-10 pt-4 md:mt-24 md:p-5 lg:mt-28 lg:pt-8 bg-white'>
+    {/* bg-[#181818] */}
+             <div className='grid grid-cols-2 md:grid-cols-6 lg:grid-cols-7 gap-4 lg:gap-6'>
+             <select className="select w-full  rounded-2xl p-3 h-12"
              value={category} // Set the current selected value
              onChange={handleCategoryChange} // Handle change
              >
-             <option disabled value="">Filter by Category</option>
+             <option disabled value="">Select by Category</option>
              <option value="Electric">Electrice</option>
              <option value="suv">SUV</option>
              <option value="Sedan">Sedan</option>
@@ -97,8 +103,8 @@ const Cars: React.FC = () => {
 
          
 
-     <select className="select w-full bg-[#111010] text-white  rounded-full p-3 h-12">
-             <option disabled value="">Seats</option>
+     <select className="select w-full  rounded-2xl p-3 h-12">
+             <option disabled value="">Select by Seats</option>
              <option value="4">4 or more</option>
              <option value="5">5 or more</option>
              <option value="6">6 or more</option>
@@ -108,7 +114,7 @@ const Cars: React.FC = () => {
     
 
            <select
-                    className="select w-full bg-[#111010] text-white  border rounded-full p-3 h-12"
+                    className="select w-full  rounded-2xl p-3 h-12"
                     value={minPrice && maxPrice ? `${minPrice}-${maxPrice}` :""}
                     onChange={handlePriceRangeChange}
                 >
@@ -123,7 +129,7 @@ const Cars: React.FC = () => {
 
 
                 <select
-    className="select w-full bg-[#111010] text-white  border rounded-full p-3 h-12"
+    className="select w-full  rounded-2xl p-3 h-12"
     value={sortOption}
     onChange={(e) => setSortOption(e.target.value)} // Handle sorting change
 >
@@ -135,54 +141,57 @@ const Cars: React.FC = () => {
  
              </div>
     
-    </div>
+    
      {/* Display car data */}
-     <ul>
+     <div className=''>
+     <div>
+     {cardata.length} + cars available
          {Array.isArray(cardata) && cardata.length > 0 ? (
-             <div className="grid mt-5 grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 bg-[#1d1c1a]">
+             <div className="grid mt-5 grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 ">
+                
                  {cardata.map((car: Car) => (
-                     <div key={car._id} className="card card-compact bg-[#272625]  shadow-xl rounded-3xl p-4">
-                         <figure>
-                             <img
-                                 src={car.image}
-                                 alt={car.name}
-                                  className="w-[95%] h-[300px] object-cover rounded-3xl"
-                             />
-                         </figure>
-                         <div className="card-body">
-                             <h2 className="card-title">{car.name}</h2>
-                             <p>{car.review}</p>
-                             <div className='flex lg:gap-60 '>
-                                 <p>Model</p>
-                                 <p>{car.model}</p>
-                             </div>
-                             <div>
-                                 <p>Avilability</p>
-                                 <p>{car.availability}</p>
-                             </div>
-                             <div>
-                                 <p>Category</p>
-                                 <p>{car.category}</p>
-                             </div>
-                             <div>
-                                 <p>Price</p>
-                                 <p>{car.price}</p>
-                             </div>
-                             <div>
-                                 <p>date</p>
-                                 <p>{formatDate(car.date)}</p> 
-                             </div>
-                             <div className="card-actions justify-end">
-                                 <button className="btn btn-primary rounded-3xl">Buy Now</button>
-                             </div>
-                         </div>
+                     <div key={car._id} className="card lg:card-side bg-base-100 shadow-xl rounded-2xl group">
+                     <figure className="w-full lg:w-[50%]"> {/* Adjust the width of the image container */}
+                       <img
+                         className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105" 
+                         src={car.image}
+                         alt={car.name}
+                       />
+                     </figure>
+                     <div className="card-body w-full lg:w-2/3 flex-1"> {/* Adjust the width for the content */}
+                       <h2 className="card-title">{car.model}</h2>
+                       
+                       {
+                        car.rating > 0 ?(<p className='flex gap-1'>{car.rating}<MdOutlineStar className='text-[#f0bb0c] mt-1'/>    ({car.trip_count}   trips)    <FaAward className='mt-1'/>   All-Star-Host</p>):
+                        (
+                            <p>New listing</p>
+                        )
+                       }
+                       <p className='flex gap-1'><FaMapLocationDot className='mt-1'/>{car.make}</p>
+                       
+                       {car.discount > 0 ? (
+      <span className="flex gap-1 text-[#469761] ">
+        <MdOutlineDiscount className=' mt-1'/> Discount: {car.discount}%
+      </span>
+    ) : (
+      <p></p>
+    )}
+                       
+                       <div className="card-actions justify-end">
+                         <span>${car.price}/day</span>
+                       </div>
                      </div>
+                   </div>
+                   
                  ))}
              </div>
          ) : (
              <li>No cars available or data format is incorrect</li>
          )}
-     </ul>
+     </div>
+
+     </div>
+    
      <div className="mx-auto text-center m-16">
                          <button className="btn btn-active btn-primary mr-4"
                              onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
