1a6116c1d33fd1d2e08ede4e09e9ed695d24cb74
Armstain <tahsin10@outlook.com>
2024-10-05 23:49:42 +0600

    overwritten issue

--- src/Components/MapComponent.tsx
+++ src/Components/MapComponent.tsx
@@ -8,7 +8,8 @@ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 // Fix Leaflet icon issue
 const fixLeafletIcons = () => {
-    delete L.Icon.Default.prototype._getIconUrl;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
     L.Icon.Default.mergeOptions({
         iconRetinaUrl: markerIcon2x,
         iconUrl: markerIcon,
