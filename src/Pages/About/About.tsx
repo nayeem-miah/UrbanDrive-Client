1e7c62e39717f19c8aad91e8114e54f89a986896
aayman01 <ayman.alfi098@gmail.com>
2024-10-17 13:54:34 +0600

    contact and about design changed

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import slide1 from '../../assets/slides/slide1.jpg';
-import slide2 from '../../assets/slides/slide2.jpg';
-import slide3 from '../../assets/slides/slide3.jpg';
-import member1 from '../../assets/About-us/member1.jpg';
-import member2 from '../../assets/About-us/member2.jpg';
-import member3 from '../../assets/About-us/member3.jpg';
+import React, { useEffect, useState } from "react";
+import member1 from "../../assets/About-us/member1.jpg";
+import member2 from "../../assets/About-us/member2.jpg";
+import member3 from "../../assets/About-us/member3.jpg";
 
-import './style.css'
+import "./style.css";
 
 const AboutUs: React.FC = () => {
   const [scrollY, setScrollY] = useState(0);
@@ -16,10 +13,10 @@ const AboutUs: React.FC = () => {
       setScrollY(window.scrollY);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -31,104 +28,154 @@ const AboutUs: React.FC = () => {
 
   return (
     <div className="about-us">
-      <section 
-        className="bg-cover bg-center h-screen flex items-center justify-center relative overflow-hidden"
-        style={{ 
-          backgroundImage: `url(${slide1})`,
-          backgroundPositionY: `${scrollY * 0.5}px`
-        }}
-      >
-        <div className="text-center z-10 flex flex-col px-4">
-          <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-white font-Playfair">About <span className='text-primary'>Us</span></h1>
-          <p className='text-white mt-4 sm:mt-8 text-sm sm:text-lg font-Merri font-bold w-full sm:w-3/4 mx-auto'>Our mission is to be the leading car rental provider,
-          delivering exceptional service, prioritizing safety, and offering unparalleled convenience to our customers, making every journey a memorable one.</p>
-          <p className="text-white mt-8 sm:mt-16 text-sm sm:text-lg font-Merri font-bold">Learn more about our journey and values.</p>
+      <>
+        <div
+          className="background-container flex items-center justify-center overflow-hidden"
+          style={{
+            // backgroundImage: `url(${slide1})`,
+            backgroundPositionY: `${scrollY * 0.5}px`,
+          }}
+        >
+          <div className="overlay"></div>
+
+          <div className="text-center content  flex flex-col px-4">
+            <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-white font-Playfair">
+              About <span className="text-primary">Us</span>
+            </h1>
+            <p className="text-white mt-4 sm:mt-8 text-sm sm:text-lg font-Merri font-bold w-full sm:w-3/4 mx-auto">
+              Our mission is to be the leading car rental provider, delivering
+              exceptional service, prioritizing safety, and offering
+              unparalleled convenience to our customers, making every journey a
+              memorable one.
+            </p>
+            <p className="text-white mt-8 sm:mt-16 text-sm sm:text-lg font-Merri font-bold">
+              Learn more about our journey and values.
+            </p>
+          </div>
+          <div className=" absolute bottom-10 transform -translate-x-1/2 left-1/2 h-5">
+            <div className="arrow"></div>
+          </div>
         </div>
-        <div className=" absolute bottom-10 transform -translate-x-1/2 left-1/2 h-5">
-          <div className="arrow"></div>
-        </div>
-      </section>
+      </>
 
       <section className="py-12 sm:py-16 px-4 md:px-16">
         <div className="container mx-auto">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">Our <span className='text-primary'>Vision</span></h2>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">
+            Our <span className="text-primary">Vision</span>
+          </h2>
           <p className="mt-4 sm:mt-6 text-center max-w-3xl mx-auto text-base sm:text-lg text-gray-500 font-Merri">
-          To become the leading car rental company known for quality, innovation, and commitment to our customers and community.
+            To become the leading car rental company known for quality,
+            innovation, and commitment to our customers and community.
           </p>
         </div>
       </section>
 
-      <section 
-        className="py-24 sm:py-36 px-4 md:px-16 relative bg-cover bg-center"
-        style={{ 
-          backgroundImage: `url(${slide2})`,
-          backgroundAttachment: 'fixed'
+      <section
+        className="py-24 sm:py-36 px-4 md:px-16 background-container"
+        style={{
+          // backgroundImage: `url(${slide2})`,
+          backgroundAttachment: "fixed",
         }}
       >
-        <div className="container mx-auto relative z-10">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">Our <span className='text-primary'>Values</span></h2>
+        <div className="overlay"></div>
+        <div className="container mx-auto content">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">
+            Our <span className="text-primary">Values</span>
+          </h2>
           <p className="mt-4 sm:mt-6 text-center max-w-3xl mx-auto text-base sm:text-lg text-white font-Merri">
-            To provide exceptional car rental services that exceed customer expectations, ensuring convenience, reliability, and satisfaction in every journey.
+            To provide exceptional car rental services that exceed customer
+            expectations, ensuring convenience, reliability, and satisfaction in
+            every journey.
           </p>
         </div>
       </section>
 
       <section className="py-12 sm:py-16 px-4 md:px-16">
         <div className="container mx-auto">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-black font-Playfair">Meet Our <span className='text-primary'>Team</span></h2>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-black font-Playfair">
+            Meet Our <span className="text-primary">Team</span>
+          </h2>
           <div className="mt-8 sm:mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
             {teamMembers.map((member, index) => (
               <div key={index} className="team-member text-center">
-                <img src={member.image} alt={`${member.name}`} className="w-24 h-24 sm:w-32 sm:h-32 mx-auto rounded-full object-cover" />
-                <h3 className="text-lg sm:text-xl font-bold mt-4 text-black">{member.name}</h3>
-                <p className="text-gray-400">{member.role}</p>
+                <img
+                  src={member.image}
+                  alt={`${member.name}`}
+                  className="w-24 h-24 sm:w-32 sm:h-32 mx-auto rounded-full object-cover"
+                />
+                <h3 className="text-lg sm:text-xl font-bold mt-4 text-black">
+                  {member.name}
+                </h3>
+                <p className="text-gray-500">{member.role}</p>
               </div>
             ))}
           </div>
         </div>
       </section>
 
-      <section 
-        className="py-12 sm:py-16 px-4 md:px-16 relative bg-cover bg-center"
-        style={{ 
-          backgroundImage: `url(${slide3})`,
-          backgroundAttachment: 'fixed'
+      <section
+        className="py-12 sm:py-16 px-4 md:px-16 background-container"
+        style={{
+          // backgroundImage: `url(${slide3})`,
+          backgroundAttachment: "fixed",
         }}
       >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="container mx-auto relative z-10">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">Our Story</h2>
+        <div className="overlay"></div>
+        <div className="container mx-auto content">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">
+            Our Story
+          </h2>
           <p className="mt-4 sm:mt-6 max-w-3xl mx-auto text-base sm:text-lg text-center text-white font-Merri">
-            Founded in 2024, UrbanDrive has quickly grown to become a trusted name in car rentals, known for our dedication to customer service and high-quality vehicles.
+            Founded in 2024, UrbanDrive has quickly grown to become a trusted
+            name in car rentals, known for our dedication to customer service
+            and high-quality vehicles.
           </p>
         </div>
       </section>
 
-      <section className="py-12 sm:py-16 px-4 md:px-16 bg-[#111010]">
+      <section className="py-12 sm:py-16 px-4 md:px-16">
         <div className="container mx-auto">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair">Our Core <span className='text-teal-400'>Values</span></h2>
-          <div className="mt-8 sm:mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            {['Integrity', 'Trust', 'Honesty'].map((value) => (
-              <div key={value} className="value-item text-center">
-                <h3 className="text-xl sm:text-2xl font-bold text-white font-Merri">{value}</h3>
-                <p className="mt-2 sm:mt-4 text-gray-400 font-Open">We act with {value.toLowerCase()} in all that we do.</p>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-black font-Playfair">
+            Our Core <span className="text-primary">Values</span>
+          </h2>
+          <div className="mt-8 sm:mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 ">
+            {["Integrity", "Trust", "Honesty"].map((value) => (
+              <div
+                key={value}
+                className="value-item text-center border-primary border-2 px-4 py-6 rounded"
+              >
+                <h3 className="text-xl sm:text-2xl font-bold text-black font-Merri">
+                  {value}
+                </h3>
+                <p className="mt-2 sm:mt-4 text-gray-500 font-Open">
+                  We act with {value.toLowerCase()} in all that we do.
+                </p>
               </div>
             ))}
           </div>
         </div>
       </section>
-      <hr className='border-teal-500 border-2' />
+      <hr className="border-primary border-2" />
 
-      <section className="py-12 sm:py-16 px-4 md:px-16 bg-[#111010]">
+      <section className="py-12 sm:py-16 px-4 md:px-16">
         <div className="container mx-auto text-center">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-white font-Playfair">Get in Touch</h2>
-          <p className="mt-4 sm:mt-6 max-w-3xl mx-auto text-base sm:text-lg text-gray-300">
-            Have questions? Reach out to us at <a href="mailto:info@urbandrive.com" className="text-teal-400 hover:text-teal-300">info@urbandrive.com</a>.
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-black font-Playfair">
+            Get in Touch
+          </h2>
+          <p className="mt-4 sm:mt-6 max-w-3xl mx-auto text-base sm:text-lg text-gray-500">
+            Have questions? Reach out to us at{" "}
+            <a
+              href="mailto:info@urbandrive.com"
+              className="text-primary hover:text-secondary"
+            >
+              info@urbandrive.com
+            </a>
+            .
           </p>
         </div>
       </section>
     </div>
-  );    
+  );
 };
 
 export default AboutUs;
--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import slide1 from '../../assets/slides/slide1.jpg';
+import slide1 from '../../assets/nissan.jpg';
 import { MdEmail, MdLocationOn, MdAccessTime } from 'react-icons/md';
-import slide2 from '../../assets/slides/slide2.jpg';
+import slide2 from "../../assets/nissan.jpg";
+import './style.css';
 
 const Contact: React.FC = () => {
   const [scrollY, setScrollY] = useState(0);
@@ -23,51 +24,51 @@ const Contact: React.FC = () => {
   ];
 
   return (
-    <div className='contact bg-[#18181B]'>
-      <section className='relative'>
+    <div className="contact">
+      <section className="relative">
         <div
           className="bg-cover bg-center h-[60vh] flex items-center justify-center relative overflow-hidden"
           style={{
             backgroundImage: `url(${slide1})`,
-            backgroundPositionY: `${scrollY * 0.5}px`
+            backgroundPositionY: `${scrollY * 0.5}px`,
           }}
         >
-          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute inset-0"></div>
           <div className="text-center z-10 flex flex-col px-4">
             <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-white font-Playfair">
-              Contact <span className='text-teal-400'>Us</span>
+              Contact <span className="text-primary">Us</span>
             </h1>
             <p className="text-white mt-4 sm:mt-8 text-sm sm:text-lg font-Merri font-bold">
               Get in touch with us for any inquiries
             </p>
           </div>
         </div>
-        
-        <div className='lg:absolute lg:bottom-0 lg:left-0 lg:right-0 lg:transform lg:translate-y-1/2'>
-          <div className='container mx-auto px-4'>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6'>
+
+        <div className="lg:absolute lg:bottom-0 lg:left-0 lg:right-0 lg:transform lg:translate-y-1/2">
+          <div className="container mx-auto px-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
               {contactInfo.map((item, index) => (
-                <div 
+                <div
                   key={index}
-                  className={`rounded-lg p-6 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-[#18181B] hover:bg-teal-400 group`}
+                  className={`rounded-lg p-6 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-white hover:bg-primary group`}
                 >
                   <div className="flex flex-col items-center justify-center text-center group-hover:text-[#18181B]">
-                    <div className="mb-4 transition-colors duration-300 text-teal-400 group-hover:text-[#18181B]">
+                    <div className="mb-4 transition-colors duration-300 text-primary group-hover:text-[#18181B]">
                       {item.icon}
                     </div>
                     <h2 className="text-lg font-bold mt-2">{item.title}</h2>
-                    {item.title === 'Email us' ? (
-                      <a 
-                        href={`mailto:${item.content}`} 
+                    {item.title === "Email us" ? (
+                      <a
+                        href={`mailto:${item.content}`}
                         className="text-sm  hover:text-[#18181B]"
                       >
                         {item.content}
                       </a>
-                    ) : item.title === 'Our address' ? (
-                      <a 
-                        href={`https://www.google.com/maps/search/?api=1&query=${(item.content)}`} 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
+                    ) : item.title === "Our address" ? (
+                      <a
+                        href={`https://www.google.com/maps/search/?api=1&query=${item.content}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className="text-sm  hover:text-[#18181B]"
                       >
                         {item.content}
@@ -75,7 +76,6 @@ const Contact: React.FC = () => {
                     ) : (
                       <p className="text-sm">{item.content}</p>
                     )}
-                      
                   </div>
                 </div>
               ))}
@@ -83,12 +83,13 @@ const Contact: React.FC = () => {
           </div>
         </div>
       </section>
-      <hr className='border-teal-400 border-2 mt-32' />
-      <section className="mt-24 bg-[#18181B]">
+      <hr className="border-primary border-2 mt-32" />
+      <section className="mt-24">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold font-Playfair text-white mb-8 text-center">Get In <span className='text-teal-400'>Touch</span></h2>
+          <h2 className="text-3xl font-bold font-Playfair text-black mb-8 text-center">
+            Get In <span className="text-primary">Touch</span>
+          </h2>
           <div className="flex flex-col md:flex-row gap-8">
-
             {/* Contact Form */}
             <div className="w-full md:w-1/2">
               <form>
@@ -96,91 +97,92 @@ const Contact: React.FC = () => {
                   <input
                     type="text"
                     placeholder="Your Name*"
-                    className="w-full p-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                    className="w-full p-3 border rounded-lg text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-primary"
                   />
                   <input
                     type="email"
                     placeholder="Your Email*"
-                    className="w-full p-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                    className="w-full p-3 border rounded-lg text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-primary"
                   />
                   <input
                     type="tel"
                     placeholder="Your Number*"
-                    className="w-full p-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                    className="w-full p-3 border rounded-lg text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-primary"
                   />
                   <input
                     type="text"
                     placeholder="Subject*"
-                    className="w-full p-3 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                    className="w-full p-3 border rounded-lg text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-primary"
                   />
                 </div>
                 <textarea
                   placeholder="Your Message"
                   rows={5}
-                  className="w-full p-3 mt-4 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                  className="w-full p-3 mt-4 border rounded-lg text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-primary"
                 ></textarea>
                 <button
                   type="submit"
-                  className="inline-flex items-center justify-center w-full px-4 py-2 mt-4 text-gray-900 rounded-lg group bg-gradient-to-br from-teal-500
-       to-navy-700 group-hover:from-teal-500 group-hover:to-navy-700 hover:text-white dark:text-white focus:ring-1 focus:outline-none focus:ring-white
-       dark:focus:ring-white hover:bg-[#12625D] font-Merri font-bold"
+                  className="inline-flex items-center justify-center w-full px-4 py-2 mt-4w-full bg-gradient-to-r from-[#3d83d3] to-[#a306fd] text-white font-bold rounded hover:bg-blue-600"
                 >
                   Submit
                 </button>
               </form>
             </div>
-            
+
             {/* Map */}
             <div className="w-full md:w-1/2">
-  <div className="h-[400px] rounded-2xl overflow-hidden">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14599.241897333992!2d90.41001779011839!3d23.82533761273339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1727014318933!5m2!1sen!2sbd"
-      className="w-full h-full border-0"
-      allowFullScreen
-      loading="lazy"
-    ></iframe>
-  </div>
-  <div className="mt-2 text-white flex justify-center">
-    <p>Dhaka, Bangladesh</p>
-  </div>
-</div>
-
+              <div className="h-[400px] rounded-2xl overflow-hidden">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14599.241897333992!2d90.41001779011839!3d23.82533761273339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1727014318933!5m2!1sen!2sbd"
+                  className="w-full h-full border-0"
+                  allowFullScreen
+                  loading="lazy"
+                ></iframe>
+              </div>
+              <div className="mt-2 text-white flex justify-center">
+                <p>Dhaka, Bangladesh</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
       {/* <hr className='border-teal-400 border-2 mt-16' /> */}
-      <section 
-        className="pt-24 mt-10 sm:py-36 px-4 md:px-16 relative bg-cover bg-center"
-        style={{ 
+      <section
+        className="pt-24 mt-10 sm:py-36 px-4 md:px-16 background-container"
+        style={{
           backgroundImage: `url(${slide2})`,
-          backgroundAttachment: 'fixed'
+          backgroundAttachment: "fixed",
         }}
       >
-        <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="container mx-auto relative z-10">
-          <p className='text-teal-400 font-Playfair text-sm  text-center mb-2 tracking-wider'>'Rent Your Car</p>
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair tracking-wider">Interested In Renting A Car?</h2>
-          <p className="mt-4 sm:mt-6 text-center max-w-3xl mx-auto text-base sm:text-sm text-white font-Merri">
-          Don't hesitate and send us a message.
+        <div className="overlay"></div>
+        <div className="container mx-auto content">
+          <p className="text-primary font-Playfair text-sm  text-center mb-2 tracking-wider">
+            'Rent Your Car
+          </p>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-center text-white font-Playfair tracking-wider">
+            Interested In Renting A Car?
+          </h2>
+          <p className="mt-4 sm:mt-6 text-center max-w-3xl mx-auto text-base sm:text-sm text-gray-500 font-Merri">
+            Don't hesitate and send us a message.
           </p>
 
-          <div className="flex justify-center mt-8 pb-8 space-x-4">
-  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-500 to-navy-700 group-hover:from-teal-500 group-hover:to-navy-700 hover:text-white dark:text-white focus:ring-2 focus:outline-none focus:ring-white dark:focus:ring-white">
-    <span className="relative px-3 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 font-Open">
-      Live Chat    
-    </span>
-  </button>
+          <div className="flex justify-center mt-8 pb-4 space-x-4">
+            <button className="hover:text-primary px-2 hover:bg-white mb-2 overflow-hidden text-base sm:text-lg  text-white bg-primary rounded-lg font-bold">
+              {/* <span className="relative transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 font-Open">
+                Live Chat
+              </span> */}
+              Live Chat
+            </button>
 
-  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-teal-500 rounded-lg group hover:text-white border border-teal-500 focus:ring-2 focus:outline-none focus:ring-white dark:focus:ring-white">
-    <span className="relative px-3 py-1 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-teal-500 font-Open">
-      Rent Now
-    </span>
-  </button>
-</div>
-
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-base sm:text-lg font-medium text-white rounded-lg group hover:text-white border-2 border-primary focus:ring-yellow-400 dark:focus:ring-yellow-800">
+              <span className="relative px-3 sm:px-4 lg:px-4 py-2 sm:py-3 lg:py-[18px] transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-primary font-Open font-bold">
+                Rent Now
+              </span>
+            </button>
+          </div>
         </div>
       </section>
-      <hr className='border-teal-400 border-2 ' />
+      <hr className="border-primary border-2 " />
     </div>
   );
 };
